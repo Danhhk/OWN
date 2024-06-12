935ee9792c8e7ce9ee4a229f7a9e10deff7ced8c
Dan <yushchenko9901@gmail.com>
2024-06-13 00:55:03 +0300

    Все циклы и условия были переписаны в функции, и было добалено функцию showMyDB() и writeYourGenres()

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 "use strict"
 
-const numberOfFilms = +prompt("How many movies you've watched?", "")
+let numberOfFilms;
     
+function start() {
+    numberOfFilms = +prompt("How many movies you've watched?", "")
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies you've watched?", "")
+    }
+}
+
+// start()
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -11,49 +22,57 @@ const personalMovieDB = {
 }
 
 
-for (let i = 0; i < 2; i++) {
-    const last_viewed_movie = prompt("What was the last movie you watch?", ""),
-          movie_score = prompt("How much would you rate it?", "0.0")
+function rememberMyFilms() {
+    
+    for (let i = 0; i < 2; i++) {
+        const last_viewed_movie = prompt("What was the last movie you watch?", ""),
+            movie_score = prompt("How much would you rate it?", "0.0")
 
-    if (last_viewed_movie != null && movie_score != null &&
-        last_viewed_movie != '' && movie_score != '' &&
-        last_viewed_movie.length < 50 && movie_score.length < 10) {
+        if (last_viewed_movie != null && movie_score != null &&
+            last_viewed_movie != '' && movie_score != '' &&
+            last_viewed_movie.length < 50 && movie_score.length < 10) {
 
-        personalMovieDB.movies[last_viewed_movie] = movie_score
-        console.log('done')
+            personalMovieDB.movies[last_viewed_movie] = movie_score
+            console.log('done')
+        } else {
+            console.log('error')
+            i--
+        }   
+    }
+}
+
+// rememberMyFilms()
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("You've seen a lot of movies!")
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        console.log("You're a cool viewer!")
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You're a movie buff!")
     } else {
-        console.log('error')
-        i--
-    }   
+        console.log('Error')
+    }
 }
 
-// let i = 0 // Не работает
+// detectPersonalLevel()
 
-// while (i <= 2) {
-//     const last_viewed_movie = prompt("What was the last movie you watch?", ""),
-//           movie_score = prompt("How much would you rate it?", "0.0")
 
-//     if (last_viewed_movie != null && movie_score != null &&
-//         last_viewed_movie != '' && movie_score != '' &&
-//         last_viewed_movie.length < 50 && movie_score.length < 10) {
-
-//         personalMovieDB.movies[last_viewed_movie] = movie_score
-//         console.log('done')
-//     } else {
-//         console.log('error')
-//         i--
-//     }
-//     i++
-// }
-
-if (personalMovieDB.count < 10) {
-    console.log("You've seen a lot of movies!")
-} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-    console.log("You're a cool viewer!")
-} else if (personalMovieDB.count >= 30) {
-    console.log("You're a movie buff!")
-} else {
-    console.log('Error')
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB)
+    }
 }
 
-console.log(personalMovieDB)
+showMyDB(personalMovieDB.privat)
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Your favorite genre is numbered ${i}`)
+    }
+}
+
+writeYourGenres()
+
+// console.log(personalMovieDB)
